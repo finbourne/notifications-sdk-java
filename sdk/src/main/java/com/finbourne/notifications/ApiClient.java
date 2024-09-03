6b46e62aca2bca1a46e9054183364349fb4a6382
Concourse <concourse@finbourne.com>
2024-09-03 13:42:09 +0000

    notifications java updates for v2.0.62
    Signed-off-by: Concourse <concourse@finbourne.com>

--- sdk/src/main/java/com/finbourne/notifications/ApiClient.java
+++ sdk/src/main/java/com/finbourne/notifications/ApiClient.java
@@ -212,7 +212,7 @@ public class ApiClient {
         json = new JSON();
 
         // Set default User-Agent.
-        setUserAgent("OpenAPI-Generator/2.0.61/java");
+        setUserAgent("OpenAPI-Generator/2.0.62/java");
 
         authentications = new HashMap<String, Authentication>();
     }
